61d0c3a34b4d61cb6e2ec186604e5e882d155226
smallfishit <henrychang0202@gmail.com>
2022-04-23 13:06:13 +0800

    測試資料方法修正

--- TestProject/FeeTest.cs
+++ TestProject/FeeTest.cs
@@ -9,41 +9,40 @@ using TestProject.Models;
 
 namespace TestProject
 {
+
     [TestFixture]
     public class FeeTest
     {
-        [Test]
-        public void GetFeeFromDate_InputDateTime_ReturnSum()
+        /// <summary>停車費測試資料</summary>
+        private static FeeTestData[] _feeTestData = new[]
         {
-            List<FeeTestData> feeList = new List<FeeTestData>();
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 0, 0, 50), endTime = new DateTime(2022, 4, 20, 0, 9, 40), hours = 0, minutes = 9, fee = 0},
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 0, 5, 50), endTime = new DateTime(2022, 4, 20, 0, 15, 40), hours = 0, minutes = 10, fee = 0},
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 9, 5, 50), endTime = new DateTime(2022, 4, 20, 9, 30, 40), hours = 0, minutes = 25, fee = 2},
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 9, 5, 50), endTime = new DateTime(2022, 4, 20, 9, 35, 40), hours = 0, minutes = 30, fee = 2},
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 9, 25, 50), endTime = new DateTime(2022, 4, 20, 10, 15, 40), hours = 0, minutes = 50, fee = 5},
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 8, 15, 50), endTime = new DateTime(2022, 4, 20, 9, 40, 40), hours = 1, minutes = 25, fee = 7},
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 8, 15, 50), endTime = new DateTime(2022, 4, 20, 10, 0, 40), hours = 1, minutes = 45, fee = 10},
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 8, 15, 50), endTime = new DateTime(2022, 4, 20, 10, 35, 40), hours = 2, minutes = 20, fee = 12},
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 8, 15, 50), endTime = new DateTime(2022, 4, 20, 11, 5, 40), hours = 2, minutes = 50, fee = 15},
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 8, 15, 50), endTime = new DateTime(2022, 4, 20, 11, 25, 40), hours = 3, minutes = 10, fee = 17},
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 8, 15, 50), endTime = new DateTime(2022, 4, 20, 11, 50, 40), hours = 3, minutes = 35, fee = 20},
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 8, 15, 50), endTime = new DateTime(2022, 4, 20, 13, 40, 40), hours = 5, minutes = 25, fee = 27},
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 8, 15, 50), endTime = new DateTime(2022, 4, 20, 14, 10, 40), hours = 5, minutes = 55, fee = 30},
+            new FeeTestData(){startTime= new DateTime(2022, 4, 20, 8, 15, 50), endTime = new DateTime(2022, 4, 20, 15, 40, 40), hours = 7, minutes = 25, fee = 30}
+        };
 
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 0, 0, 50), new DateTime(2022, 4, 20, 0, 9, 40), 0, 9, 0));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 0, 5, 50), new DateTime(2022, 4, 20, 0, 15, 40), 0, 10, 0));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 9, 5, 50), new DateTime(2022, 4, 20, 9, 30, 40), 0, 25, 2));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 9, 5, 50), new DateTime(2022, 4, 20, 9, 35, 40), 0, 30, 2));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 9, 25, 50), new DateTime(2022, 4, 20, 10, 15, 40), 0, 50, 5));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 9, 5, 50), new DateTime(2022, 4, 20, 10, 5, 40), 1, 0, 5));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 8, 15, 50), new DateTime(2022, 4, 20, 9, 40, 40), 1, 25, 7));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 8, 15, 50), new DateTime(2022, 4, 20, 10, 0, 40), 1, 45, 10));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 8, 15, 50), new DateTime(2022, 4, 20, 10, 35, 40), 2, 20, 12));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 8, 15, 50), new DateTime(2022, 4, 20, 11, 5, 40), 2, 50, 15));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 8, 15, 50), new DateTime(2022, 4, 20, 11, 25, 40), 3, 10, 17));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 8, 15, 50), new DateTime(2022, 4, 20, 11, 50, 40), 3, 35, 20));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 8, 15, 50), new DateTime(2022, 4, 20, 13, 40, 40), 5, 25, 27));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 8, 15, 50), new DateTime(2022, 4, 20, 14, 10, 40), 5, 55, 30));
-            feeList.Add(new FeeTestData(new DateTime(2022, 4, 20, 8, 15, 50), new DateTime(2022, 4, 20, 15, 40, 40), 7, 25, 30));
-
-            foreach (FeeTestData feeData in feeList)
-            {
-                //建立停車費計算物件
-                ParkingFeeCalculator parkFee = new ParkingFeeCalculator(feeData.startTime, feeData.endTime);
-                //計算停車費
-                var result = parkFee.GetFeeFromDate();
-                //驗證結果是否正確
-                Assert.AreEqual(feeData.hours, result.hours);
-                Assert.AreEqual(feeData.minutes, result.minutes);
-                Assert.AreEqual(feeData.fee, result.fee);
-            }
+        [Test]
+        public void GetFeeFromDate_InputDateTime_ReturnSum([ValueSource("_feeTestData")]FeeTestData feeData)
+        {
+            //建立停車費計算物件
+            ParkingFeeCalculator parkFee = new ParkingFeeCalculator(feeData.startTime, feeData.endTime);
+            //計算停車費
+            var result = parkFee.GetFeeFromDate();
+            //驗證結果是否正確
+            Assert.AreEqual(feeData.hours, result.hours);
+            Assert.AreEqual(feeData.minutes, result.minutes);
+            Assert.AreEqual(feeData.fee, result.fee);
         }
     }
 }
