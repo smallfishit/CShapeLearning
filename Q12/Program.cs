75d2b34531112ca43214bca8d33c3844f1400c7e
smallfishit <henrychang0202@gmail.com>
2022-04-24 13:51:31 +0800

    第12題：九九乘法表

---
+++ Q12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    if(j > 1)
+                    {
+                        Console.Write("  ");
+                    }
+                    string space = "";
+                    if (i*j < 10)
+                    {
+                        space = " ";
+                    }
+                    Console.Write($"{i}*{j}={space}{i * j}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("請輸入任意鍵結束!");
+            Console.ReadKey();
+        }
+    }
+}
